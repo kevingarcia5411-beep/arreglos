ff38405a131b2ce1b04ad49576b7aa01e65b5c54
kevingarcia5411-beep <kevin.garcia5411@alumnos.udg.mx>
2026-03-06 08:23:15 -0600

    Add files via upload

---
+++ arreglos 01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    float temp1, temp2;
+    float temp[24];
+    int dummy;
+
+    printf("direccion de mem de 2 flotantes: &temp1 : %i, \n&temp2 %i!\n", &temp1, &temp2);
+    printf ("direccion de temp[24] :%i,\n", temp);
+    printf ("direcion de memoria de %temp[24]:%i,\n", &temp);
+    printf ("direccion de &dummy:%i\n", &dummy);
+
+
+    printf("%f\n", temp[0]=20.5);
+    printf("%f\n", temp[0]=18.2);
+    printf("%f\n", temp[0]);
+
+
+    return 0;
+
+
+    /* al ejecturar el programa nos arroja la direccion de memoria, ahi se ve el espacio que ocupa cada dato
+    y se puede observar como en el arreglo "temp[24] la direccion de memoria es un bloque mas abajo */
+
+
+}
